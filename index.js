26bd7e8016d3988c73cc5422fe9317e5f435f858
Guilherme de Oliveira <92833722+guix7@users.noreply.github.com>
2021-10-21 21:41:40 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const linksSocialMedia = {
+  github: 'guix7',
+  youtube: 'Guilherme Oliveira',
+  facebook: 'facebook',
+  instagram: 'gui123_j',
+  twitter: 'oliveira_guix'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+changeSocialMediaLinks()
+
+function getGithubProfileInfo() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+    })
+}
+
+getGithubProfileInfo()
